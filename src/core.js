5ef926903f185cf309b0cac3ae336ff7a3c2b75e
adam <adam.thorpe.g@gmail.com>
2016-08-28 14:54:39 -0600

    Update App and Panel components.

--- src/core.js
+++ src/core.js
@@ -1,7 +1,18 @@
 'use strict';
 
+//
+// Components
+// ---------------------------------------
+import App from './components/app.jsx';
+import Panel from './components/panel.jsx';
+
+//
+// Decorators
+// ---------------------------------------
 import Webcomponent from './decorators/webcomponent.jsx';
 
 export {
+  App,
+  Panel,
   Webcomponent
 };
